bd6f653f7318c0f3198606f239b8ea1aeb3eec83
Elvis Tombo <elvistombo@gmail.com>
2026-06-03 16:54:42 +0000

    example main control

---
+++ student.py
@@ -0,0 +1,7 @@
+class student():
+    def __init__(self,name,id):
+        self.name = name
+        self.id = id
+
+        def __str__(self):
+            return f"name:"
---
+++ test.py
@@ -0,0 +1,6 @@
+
+def main()
+    print("main function from test.py called")
+
+
+    if __name__ == "__main__"
