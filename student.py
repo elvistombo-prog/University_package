b49a654884fc0a4a9adf0f101456bc6d7d563fec
Elvis Tombo <elvistombo@gmail.com>
2026-06-10 18:02:56 +0200

    Refactor student class and add course methods
    Refactor student class to Student and add methods for course management and GPA retrieval.

--- student.py
+++ student.py
@@ -1,7 +1,27 @@
-class student():
-    def __init__(self,name,id):
-        self.name = name
-        self.id = id
 
-        def __str__(self):
-            return f"name:"
+
+class Student():
+    
+    """Represents a student wit basic details."""
+
+    
+    def __init__(self, name, id ):
+        self.name = name
+        self.id = id 
+
+
+def add_courses(self, course_id):
+
+
+
+
+
+def get_gpa(self):
+    pass
+
+def __str__(self):
+    s = f"name: {self.name}, id:{self.id}\n"
+    s += f"GPA: {self.get_gpa()}\n"
+    s += f"Courses: {str(self.courses)}"
+    s += "=========
+
